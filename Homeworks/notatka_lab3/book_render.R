041c762e55e4c812d1dd6bd308cb11a3d87b8956
wojciechwojnar <39466006+wojciechwojnar@users.noreply.github.com>
2020-11-08 17:15:31 +0100

    sorting out order

--- Homeworks/notatka_lab3/book_render.R
+++ Homeworks/notatka_lab3/book_render.R
@@ -1,4 +1,4 @@
 library(bookdown)
 # ?render_book
-render_book(input = c('formaty_danych.Rmd','wczytywanie_plikow_notatka.rmd',
-              'locales.RMD', 'rds_rda.rmd'), 'bookdown::gitbook')
+render_book(input = c('01-formaty_danych.Rmd','02-wczytywanie_plikow_notatka.rmd',
+              '03-locales.Rmd', '04-rds_rda.rmd'), 'bookdown::gitbook')
