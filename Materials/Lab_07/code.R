6778da36fc3b12ae9c4d92455e4f2345efb86c9b
Mateusz Staniak <mtst@mstaniak.pl>
2020-11-24 00:11:25 +0100

    added code from lab 7

--- Materials/Lab_07/code.R
+++ Materials/Lab_07/code.R
@@ -1,72 +1,129 @@
+x_str <- letters[1:10]
+x_fac <- as.factor(x_str)
+
+x_fac[1:5]
+
+df = data.frame(y = runif(100),
+                x = factor(sample(letters[1:10], 100, replace = TRUE)))
+
+aggregate(y ~  x, df, FUN = mean)
+
+df2 = df[df$x %in% c("a", "b", "c"), ]
+levels(df2$x)
+
+aggregate(y ~  x, df[df$x %in% c("a", "b", "c"), ], FUN = mean)
+aggregate(y ~  x, df[df$x %in% c("a", "b", "c"), ], FUN = mean, drop = F)
+
+
+model.matrix(y ~ 0 + x, df2)
+
+df3 = df2
+df3$x = factor(df3$x)
+levels(df3$x)
+
+model.matrix(y ~ 0 + x, df3)
+
+df4 = df3
+df4$x = factor(df4$x, ordered = TRUE, levels = c("c", "b", "a"))
+
+model.matrix(y ~ 0 + x, df4)
+aggregate(y ~  x, df4, FUN = mean)
+
+# df5 = df3
+# df5$x = factor(df5$x, ordered = TRUE, levels = c("c", "b", "a"),
+#                labels = c("cc", "bb", "aa"))
+# df5
+# # levels(df5$x)
+# # model.matrix(y ~ 0 + x, df5)
+
+y_fac <- as.factor(as.character(10:1))
+as.numeric(y_fac)
+
+
+x = sample(11:20, 5)
+x
+as.factor(x)
+as.numeric(as.factor(x))
+as.numeric(as.character(as.factor(x)))
+
 library(dplyr)
 library(lubridate)
-library(readr)
-
-file_paths <- list.files('.', ".csv")
-read_files <- lapply(file_paths, read_csv)
-read_files_corrected <- lapply(
-    read_files, 
-    function(df) 
-        mutate(df, `Method Code` = as.character(`Method Code`)))
-pollution <- bind_rows(read_files_corrected)
-
-library(stringr)
-
-contrast <- "KO_early-WT_late"
+library(stringr) # stringi
 
 people <- "Mateusz Staniak, 24.01.1994, Wrocław"
 str_split(people, ",")
+str_match(people, "[A-Z][a-z]+ [A-Z][a-z]+, [0-9\\.]+, [A-Z][a-zł]+")
 str_match(people, "([A-Z][a-z]+) ([A-Z][a-z]+), ([0-9\\.]+), ([A-Z][a-zł]+)")
 str_match(people, "([A-Za-z ]+), ([0-9\\.]+), ([A-Z][a-zł]+)")
 
 str_match(people, "([0-9\\.]+)")
-
-people2 <- "Mateusz Staniak"
-str_match_all(people, "[A-Z][a-zł]+")
-
+str_match(people, "[0-9\\.]+")
 
 dates <- c("7-2017", "2016-01", "12-2018", "2019-2")
 
-# I 
+# I
 dates_separated <- str_split(dates, "-")
 dates_with_zero <- lapply(dates_separated,
                           function(strvec) str_pad(strvec, width = 2, pad = "0",
                                                    side = "left"))
-fix_date_order <- function(date_vec) {
-    if(str_length(date_vec[1]) < 4) {
-      date_vec <- rev(date_vec)  
+
+dates_reordered <- lapply(
+  dates_with_zero, function(x) {
+    if (str_length(x[1]) == 2) { # opcja 2: str_length(x[1]) < str_length(x[2])
+      rev(x)
+    } else {
+      x
     }
-    paste(c(date_vec, "01"), sep = "-", collapse = "-")
+  }
+)
+
+
+fix_date_order <- function(date_vec) {
+  if(str_length(date_vec[1]) < 4) {
+    date_vec <- rev(date_vec)
+  }
+  paste(c(date_vec, "01"), sep = "-", collapse = "-")
 }
+
+
 dates <- lapply(dates_with_zero, fix_date_order)
 dates <- ymd(unlist(dates))
 class(dates)
 
+dates
 
-dates_with_zero <- lapply(
-    dates_separated,
-    fix_padding
-)
+month(dates)
 
-fix_padding <- function(strvec) str_pad(strvec, width = 2, pad = "0",
-                                        side = "left")
+year(dates)
 
-# Funkcja jako parametr
-calculate_norm <- function(vec, norm) {
-    norm(vec)
-}
-calculate_norm(c(1, 2), function(x) sqrt(sum(x^2)))
-calculate_norm(c(1, 2), function(x) sum(abs(x)))
+day(dates)
 
-power <- function(power) {
-    function(x) x^power
-}
+wday(dates, label = TRUE)
 
-power_1 <- power(1)
-power_2 <- power(2)
-power_3 <- power(3)
+datetimes <- paste(
+  as.character(dates),
+  paste(sample(0:23, 4), sample(0:59, 4), sample(0:59, 4),
+        sep = ":"))
+datetimes <- ymd_hms(datetimes, tz = "UTC")
+tz(datetimes)
 
 
-power_1(1:10)
-power_2(1:10)
-power_3(1:10)
+round_date(datetimes, unit = "hour")
+round_date(datetimes, unit = "month")
+
+ceiling_date(datetimes, unit = "hour")
+ceiling_date(datetimes, unit = "month")
+
+
+floor_date(datetimes, unit = "hour")
+floor_date(datetimes, unit = "month")
+
+month(datetimes) = month(datetimes) + 1
+datetimes
+
+datetimes[1] - datetimes[2]
+class(datetimes[1] - datetimes[2])
+
+as.numeric(datetimes[1] - datetimes[2])
+as.period(datetimes[1] - datetimes[2]) / days(1)
+as.period(datetimes[1] - datetimes[2]) / hours(1)
