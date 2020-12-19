d7c0ea68c4a5358ef7cff32bf5858e85d64f1442
dmika1234 <72747159+dmika1234@users.noreply.github.com>
2020-12-19 09:19:44 +0100

    Update Mika_Dominik.R

--- Homeworks/Homework_4/Mika_Dominik.R
+++ Homeworks/Homework_4/Mika_Dominik.R
@@ -1,1 +1,17 @@
+library(ggplot2)
+library(data.table)
+
+
+
+ggdt <- function(data, x, y, type = geom_point, theme = theme()){
+
+ggplot(data, aes(x = x, y = y)) +
+    type() +
+    theme_bw() +
+    theme
+}
+
+
+
+ggdt(gas_dt, gas_dt[,Pollutant], gas_dt[, MeasuredValue], geom_point, theme())
 
