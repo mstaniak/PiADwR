89d49e62e6fb43685035e29075d9ab82254760ba
Maciej Grabias <maciejpc4@gmail.com>
2020-12-21 15:25:19 +0100

    HW4 Update

--- Homeworks/Homework_4/grabias_maciej.R
+++ Homeworks/Homework_4/grabias_maciej.R
@@ -1,16 +1,23 @@
 library(ggplot2)
+library(data.table)
+
+# import danych
+test_data <- fread("../Homework_4/grabias_test_data.csv")
 
 # dwa mozliwe typy wykresu w mojej funkcji to "bar" i "pie"
 
 wykres_homework <- function(df, plot_type, feature, ...){
   
-  base <- ggplot(df, aes(fill = eval(parse(text = feature)), x = "n"))
- #                                                             ^^^
- #                                                  df[, n := .N, by = feature]
+  base <- ggplot(df[, n := .N, by = feature], 
+                 aes(fill = eval(parse(text = feature)), 
+                 x = "n"))
+  
+  # usuwamy "dummy column"
+  df[, n := NULL]
   
   if (plot_type == "pie"){
     
-    base + 
+      base + 
       
       geom_bar() + 
       
@@ -18,26 +25,26 @@ wykres_homework <- function(df, plot_type, feature, ...){
       
       coord_polar("y", start=0) +
       
-      scale_fill_discrete(name = feature) + # get proper legend name
+      scale_fill_discrete(name = feature) +
       
       theme(...)
   }
   
   else if (plot_type == "bar"){
     
-    base +
+      base +
       
       geom_bar(position = "dodge") +
       
       xlab(feature) +
       
-      scale_fill_discrete(name = feature) + # get proper legend name
+      scale_fill_discrete(name = feature) +
       
       theme(...)
   }
 } 
 
 # przykladowe wywolanie
-wykres_homework(df = szachy_duration,  plot_type = "bar",
+wykres_homework(df = test_data,  plot_type = "pie",
                 feature = "victory_status", legend.text = element_text(size = 20))
 
