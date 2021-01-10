c712dffb7868827232bae32abee7edd5faac2952
SebastianJachimek <73608405+SebastianJachimek@users.noreply.github.com>
2021-01-10 23:24:55 +0100

    Update momentum_function.R

--- Projects/Backtesting/scripts/momentum_function.R
+++ Projects/Backtesting/scripts/momentum_function.R
@@ -25,24 +25,20 @@ all_data <- function(tickers, start_Date, end_Date){
   rbindlist(prices)
 }
 
-start_Date <- "2010-02-02"
-end_Date <- "2020-12-20"
-
-
+start_date <- "2010-02-02"
+end_date <- "2020-12-20"
 tickers <- fread("tickers.txt")$V1
 tickers <- tolower(tickers)
-
-AllData <- all_data(tickers, start_Date, end_Date)
+full_data <- all_data(tickers, start_date, end_date)
 
 
 is_weekend <- function(date){
   ifelse(weekdays(date) %in% c("sobota", "niedziela"), TRUE, FALSE)
 }
 
-
-numdays <- function(n, date){
+numdays <- function(num, date){
   vec <- vector()
-  while (length(vec) < n){
+  while (length(vec) < num){
     if (is_weekend(as.Date(date)) == FALSE){
       vec <- c(as.Date(vec),as.Date(date))
     }
@@ -55,7 +51,7 @@ numdays <- function(n, date){
   rev(vec) 
 }
 
-slope_of_regression <- function(data, name){
+ slope_of_regression <- function(data, name){
   model <- lm(log(close)~date, data = data[ticker == name, ])
   return(c(model$coefficient[2], summary(model)$r.squared))
 }
@@ -66,58 +62,63 @@ score_function <- function(data, lst){
   for (name in lst){
     slopevec <- c(slopevec,slope_of_regression(data, name)[1])
     rsqvec <- c(rsqvec, slope_of_regression(data, name)[2])
-    
-  }
+    }
   score <- ((exp(slopevec))^252 - 1) * 100 * rsqvec
   return (score)
   
 }
-MomentumFunction <- function(data, date1, nums, portfolio, min_momentum, max_stocks, min_inv_vola, ...){
-  if (weekdays(date1) == "sobota"){
-    date1 <- date1- 1
+momentum_function <- function(data, date_of_analysis, number_of_days, portfolio, min_momentum, max_stocks, min_inv_vola, ...){
+  if (weekdays(date_of_analysis) == "sobota"){
+    date_of_analysis <- date_of_analysis - 1
   }
-  if (weekdays(date1) == "niedziela"){
-    date1 <- date1 - 2
+  if (weekdays(date_of_analysis) == "niedziela"){
+    date_of_analysis <- date_of_analysis - 2
   }
   else{
-    date1 <- date1}
-  VecOfDays <- numdays(nums, date1)
-  Window <- data[date %in% VecOfDays, ]
-  TickerList <- unique(Window$ticker)
-  Score <- score_function(Window, TickerList)
-  MomentumTab <- data.table(TickerList, Score)
-  setnames(MomentumTab, c("ticker", "Momentum"))
-  MomentumTab <- MomentumTab[order(-Momentum), ]
-  MomentumTab <- head(MomentumTab, max_stocks)
-  Price_full <- Window[ticker %in% MomentumTab$ticker & date == date1, mean(c(open, high, low, close), na.rm = TRUE), by = ticker]
-  Volatility <- Window[ticker %in% MomentumTab$ticker, sd((high - low)/close), by = ticker]
-  tmpTable <- inner_join(Price_full, Volatility, by = "ticker")
-  setnames(tmpTable, c("ticker", "Price", "Volatility"))
-  tmpTable[, "Inv Volatility"] <- tmpTable[, 1/tmpTable$Volatility]
-  MomentumTab <- inner_join(MomentumTab, tmpTable, by = "ticker")
-  MomentumTab <- MomentumTab[Momentum > min_momentum & `Inv Volatility` > min_inv_vola, ]
-  Weight <- MomentumTab$`Inv Volatility`/sum(MomentumTab$`Inv Volatility`)
-  Value1 <- portfolio * Weight
-  Quant <- Value1 / MomentumTab$Price
-  Quant2 <- floor(Quant)
-  Value2 <- Quant2 * MomentumTab$Price
-  MomentumTab[, c("Weight", "Quantity", "Value", "Date") := .(Weight, Quant2, Value2, date1)]
-  setcolorder(MomentumTab, c("ticker", "Momentum", "Volatility", "Inv Volatility", "Weight", "Price", "Quantity", "Value", "Date"))
+    date_of_analysis <- date_of_analysis}
   
-  CriticalValue <- portfolio - sum(Value2)
+  vec_of_days <- numdays(number_of_days, date_of_analysis)
+  window <- data[date %in% vec_of_days, ]
+  ticker_vec <- unique(window$ticker)
+  score <- score_function(window, ticker_vec)
+  momentum_tab <- data.table(ticker_vec, score)
+  setnames(momentum_tab, c("ticker", "momentum"))
+  momentum_tab <- momentum_tab[order(-momentum), ]
+  momentum_tab <- head(momentum_tab, max_stocks)
+  price <- window[ticker %in% momentum_tab$ticker & date == date_of_analysis, 
+                  mean(c(open, high, low, close), na.rm = TRUE), by = ticker]
+  volatility <- window[ticker %in% momentum_tab$ticker, sd((high - low)/close), by = ticker]
+  tmp_table <- data.table(inner_join(price, volatility, by = "ticker"))
+  setnames(tmp_table, c("ticker", "price", "volatility"))
+  tmp_table[, inv_volatility := 1/tmp_table[, volatility]]
+  #tmp_table[, "Inv_Volatility"] <- tmp_table[, 1/tmp_table$volatility]
+  momentum_tab <- inner_join(momentum_tab, tmp_table, by = "ticker")
+  momentum_tab <- momentum_tab[momentum > min_momentum & inv_volatility > min_inv_vola, ]
+  weight <- momentum_tab[, inv_volatility] / sum(momentum_tab[, inv_volatility])
+  value1 <- portfolio * weight
+  quant <- floor(value1 / momentum_tab[, price])
+  value2 <- quant * momentum_tab[, price]
+  
+  momentum_tab[, c("weight", "quantity", "value", "date") := 
+                 .(weight, quant, value2, date_of_analysis)]
+
+  setcolorder(momentum_tab, c("ticker", "momentum", "volatility", "inv_volatility", "weight", "price", "quantity", "value", "date"))
+  critical_value <- portfolio - sum(value2)
   ind <- 1
-  critical_index <- which(MomentumTab$Price == min(MomentumTab$Price))
-  while(CriticalValue > MomentumTab[critical_index, "Price"]){
-    MomentumTab[ind, "Quantity"] <- MomentumTab[ind, "Quantity"] + 1
-    MomentumTab[ind, "Value"] <- MomentumTab[ind, "Value"] + MomentumTab[ind, "Price"]
-    CriticalValue <- CriticalValue - MomentumTab[ind, "Price"]}
+  critical_index <- which(momentum_tab[, price] == momentum_tab[, min(price)])
+  while(critical_value > momentum_tab[critical_index, "price"]){
+    momentum_tab[ind, "quantity"] <- momentum_tab[ind, "quantity"] + 1
+    momentum_tab[ind, "value"] <- momentum_tab[ind, "value"] + momentum_tab[ind, "price"]
+    critical_value <- critical_value - momentum_tab[ind, "price"]}
     ind <- ind + 1
   
-  return (MomentumTab)
+  return (momentum_tab)
 }
 
+#TEST
 analysis_date <- ymd('2020-11-17')
 some_date <- ymd('2014-10-13')
+test1 <- momentum_function(full_data, analysis_date, 252, 20000, 40,6,12)
+test2 <- momentum_function(full_data, some_date, 155, 125000, 40,6,12)
 
-test1 <- MomentumFunction(AllData, analysis_date, 252, 20000, 40,6,12)
-test2 <- MomentumFunction(AllData, some_date, 155, 125000, 40,6,12)
+
