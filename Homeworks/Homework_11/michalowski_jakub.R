74668706086ddf26a90488e776136d76cf475c45
jakubmichalo <jakub.michaowski@gmail.com>
2021-01-19 01:54:14 +0100

    boxplot on grid added

--- Homeworks/Homework_11/michalowski_jakub.R
+++ Homeworks/Homework_11/michalowski_jakub.R
@@ -13,6 +13,7 @@ library(microbenchmark)
 library(pryr)
 library(profvis)
 
+library(cowplot)
 library(ggpubr)
 options(tidyverse.quiet = TRUE)
 library(tidyverse)
@@ -55,24 +56,46 @@ create_agg_tibble <- function(N, G){
   tib2
 }
 
-#autoplot(bench)
-#boxplot(bench)
-
-Nn <- c(100, 1000, 10000)
+Nn <- c(100, 1000, 10000, 100000)
 lista_plotow <- myplots <- vector('list', length(Nn))
 
-for(i in length(Nn)){
-  bench <- microbenchmark(
+for(i in 1:length(Nn)){
+  bench <- data.table(microbenchmark(
       dframe = create_agg_data_frame(Nn[i], 4),
       dtable = create_agg_data_table(Nn[i], 4),
       dtibble = create_agg_tibble(Nn[i], 4),
-      times = 50
-  )
-  #p <- boxplot(bench)
-  #lista_plotow[[i]] <- p
-
-  lista_plotow[[i]] <- boxplot(bench)
+      times = 40
+  ))
+  #lista_plotow[[i]] <- bench[, .(mean = mean(time), min = min(time), max = max(time)), by=expr]
+  p <- ggplot(bench, aes(y = time, x = expr, group=expr)) + geom_boxplot() + ylim(0, 1.4*10^8)
+  lista_plotow[[i]] <- p
 }
 
-#ggarrange(lista_plotow[[1]], lista_plotow[[2]], lista_plotow[[3]], nrow = 1)
-#ggarrange(boxplot(bench), boxplot(bench), boxplot(bench), nrow = 1)
+ggarrange(lista_plotow[[1]], lista_plotow[[2]], lista_plotow[[3]], lista_plotow[[4]],
+          nrow = 1, ncol = 4,
+          align = 'hv', labels = c('N=10^2', 'N=10^3', 'N=10^4', 'N=10^5'))
+
+
+# Y axis scale not udjusted ----
+lista_plotow22 <- myplots <- vector('list', length(Nn))
+
+for(i in 1:length(Nn)){
+  bench <- data.table(microbenchmark(
+      dframe = create_agg_data_frame(Nn[i], 4),
+      dtable = create_agg_data_table(Nn[i], 4),
+      dtibble = create_agg_tibble(Nn[i], 4),
+      times = 40
+  ))
+  #lista_plotow[[i]] <- bench[, .(mean = mean(time), min = min(time), max = max(time)), by=expr]
+  p <- ggplot(bench, aes(y = time, x = expr, group=expr)) + geom_boxplot()
+  lista_plotow22[[i]] <- p
+}
+cowplot::plot_grid(lista_plotow22[[1]], lista_plotow22[[2]],
+                   lista_plotow22[[3]], lista_plotow22[[4]], align = "v", ncol = 2, nrow = 2)
+
+## all combined ----
+ggarrange(ggarrange(lista_plotow[[1]], lista_plotow[[2]], lista_plotow[[3]], lista_plotow[[4]],
+                    nrow = 1, ncol = 4,
+                    align = 'hv', labels = c('N=10^2', 'N=10^3', 'N=10^4', 'N=10^5')),
+          ggarrange(lista_plotow22[[1]], lista_plotow22[[2]],
+                   lista_plotow22[[3]], lista_plotow22[[4]], align = "v", ncol = 2, nrow = 2))
